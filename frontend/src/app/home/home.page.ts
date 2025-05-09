ddfa42e406ca9e122b85d278888abeb3f1f8d046
Vitor Leal <vitorlealferra@gmail.com>
2025-05-09 15:19:47 -0300

    adicionei algums coisas que deveriam ser adicionadas as outras paginas

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent,} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonicModule, IonContent],
 })
 export class HomePage {
   constructor() {}
